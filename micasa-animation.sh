8a0bc9a85310abaeb9d8839ede7ab96268b9d958
Hannah Zafar <hannah.zafar@nasa.gov>
2025-03-20 16:45:06 -0400

    Update node options

--- micasa-animation.sh
+++ micasa-animation.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #SBATCH --account=s1460
-#SBATCH --time=0:10:00
-#SBATCH --constraint=mil
+#SBATCH --time=0:01:00
+#SBATCH --constraint="[cas|sky|mil]”
 
 module purge
 module load python/GEOSpyD/Min24.4.0-0_py3.12
