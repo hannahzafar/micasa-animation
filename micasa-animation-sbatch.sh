ab6b3934effd89632f7c25f5ad6331d501641bc0
Hannah Zafar <hannah.zafar@nasa.gov>
2025-04-03 17:13:02 -0400

    Delete old sbatch file

--- micasa-animation-sbatch.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-#SBATCH --account=s1460
-#SBATCH --time=0:01:00
-#SBATCH --constraint="[cas|sky|mil]”
-
-module purge
-module load python/GEOSpyD/Min24.4.0-0_py3.12
-
-mamba run -n data-analysis python $NOBACKUP/ghgc/micasa/micasa-animation/micasa-animation.py
